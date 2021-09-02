cab7dfabb17a7c3d640d0493fd9019064b5b65a1
synedra-datastax <77410784+synedra-datastax@users.noreply.github.com>
2021-09-02 09:50:28 -0700

    New files

---
+++ root/config/workspace/resources/edit.js
@@ -0,0 +1,31 @@
+const { createClient } = require("@astrajs/collections")
+
+const collection = 'tiktok'
+
+exports.handler = async function(event, context, callback) {
+  const astraClient = await createClient({
+    astraDatabaseId: process.env.ASTRA_DB_ID,
+    astraDatabaseRegion: process.env.ASTRA_DB_REGION,
+    username: process.env.ASTRA_DB_USERNAME,
+    password: process.env.ASTRA_DB_PASSWORD,
+  })
+const users = astraClient
+  .namespace(process.env.ASTRA_DB_KEYSPACE)
+  .collection(collection)
+  
+  const body = JSON.parse(event.body)
+  console.log(body)
+    
+    try {
+        users.update(body.userId, body.data)
+        return {
+          statusCode: 200,
+        }
+      } catch (e) {
+        console.error(e);
+        return {
+          statusCode: 500,
+          body: JSON.stringify(e),
+        }
+      }
+    }
