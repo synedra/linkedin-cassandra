718bef386898c3375a864eee38ff21b1613eb5b6
synedra-datastax <77410784+synedra-datastax@users.noreply.github.com>
2021-09-27 20:30:08 -0700

    Pushing a new image

---
+++ root/config/setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp -r /workspace/appsembler-tiktok/root/config/extensions /home/gitpod/.gitpod-code/extensions
+cp -r /workspace/appsembler-tiktok/root/config/.bashrc /home/gitpod/.bashrc.d/999-datastax
