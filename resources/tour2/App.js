c647c74957325f5265e79f45a48446f9873dfbc2
synedra-datastax <77410784+synedra-datastax@users.noreply.github.com>
2021-10-07 21:34:20 -0700

    New extension

---
+++ resources/tour2/App.js
@@ -0,0 +1,18 @@
+import { HashRouter, Route, Switch } from 'react-router-dom'
+import Home from './pages/Home'
+import Upload from './pages/Upload'
+import './App.css'
+
+const App = () => {
+  return (
+    <HashRouter>
+      <Header />
+      <Switch>
+        <Route path="/upload" component={Upload} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </HashRouter>
+  );
+}
+
+export default App
