4249dab5de203ebd5ef684a9e2a073649ec55fa2
synedra-datastax <77410784+synedra-datastax@users.noreply.github.com>
2021-10-08 15:19:04 -0700

    Adding functions

--- resources/tour2/App.js
+++ resources/tour2/App.js
@@ -1,6 +1,7 @@
 import { HashRouter, Route, Switch } from 'react-router-dom'
 import Home from './pages/Home'
 import Upload from './pages/Upload'
+import Header from './components/Header'
 import './App.css'
 
 const App = () => {
